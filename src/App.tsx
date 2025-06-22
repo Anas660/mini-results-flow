2532753e352d2251fb82507453ba3e86ae67db77
Anas660 <lookingforanas@gmail.com>
2025-06-22 23:30:37 +0500

    Initialize project with Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <span className="text-2xl font-bold text-blue-600">Vite + React + TS</span>
+    </div>
+  );
+}
+
+export default App;
